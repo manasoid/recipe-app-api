9d67e139f56bd0d0a33d4b642615b7ebdee96c7d
Mana Manna <public.static.gateway@gmail.com>
2019-02-27 22:57:33 +0900

    Fix unit test.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model, authenticate
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
@@ -35,7 +35,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password,
         )
         if not user:
-            msg = ('Unable to authenticate with provided credentials')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
