1f5cd7edef921700cfc3462c465706df523adb2d
Mana Manna <public.static.gateway@gmail.com>
2019-03-20 21:34:24 +0900

    Added ingredients feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -49,7 +49,16 @@ class ModelTests(TestCase):
         """Test the tag string representation"""
         tag = models.Tag.objects.create(
             user=sample_user(),
-            name='Vegan',
+            name='Vegan'
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
