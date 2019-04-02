0b0b46cb91c658a7242284e43b2f74e82433c9e7
Mana Manna <public.static.gateway@gmail.com>
2019-04-03 00:42:07 +0900

    Add .DS_Store to .gitignore

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,6 +40,6 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 
 class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer a recipe detail"""
+    """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
